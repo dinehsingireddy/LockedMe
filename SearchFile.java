51b5b06f978f422bec1c8538672a799092e36b32
dinehsingireddy <85833914+dinehsingireddy@users.noreply.github.com>
2021-07-17 21:28:27 +0530

    Add files via upload

---
+++ SearchFile.java
@@ -0,0 +1,25 @@
+package operations;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class SearchFile {
+
+	public void search() {
+
+
+		Scanner filename = new Scanner(System.in);
+		String file = filename.nextLine();
+
+		File file1=  new File("C:\\Users\\singi\\Downloads\\test\\" +file); 
+		if(file1.exists()) {
+			System.out.println(file1.getName() + " is found");
+		}else {
+			System.out.println("The file does not exist");
+		}
+
+
+	}
+	
+	
+}
