c09c5065b328a8ce02ae4ddcb9f38c114cb9a724
dinehsingireddy <85833914+dinehsingireddy@users.noreply.github.com>
2021-07-17 21:19:29 +0530

    Add files via upload

---
+++ WelcomeScreen.java
@@ -0,0 +1,116 @@
+package userInterface;
+
+import java.util.Scanner;
+
+import operations.AddOperation;
+import operations.DeleteFile;
+import operations.SearchFile;
+import operations.ShowAllFiles;
+import operations.WriteFile;
+
+public class WelcomeScreen {
+
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("                      Welcome to LockedMe  Application                      ");
+		System.out.println();
+		System.out.println("                               Developed by : Dinesh Reddy                      ");
+		System.out.println("                           Email Id:singireddydinesh1@gmail.com                      ");
+		
+
+		while(true) {
+
+
+			System.out.println("Enter your Choice ");
+
+			System.out.println("1. Show Files names in an ascending order present in the directory   ");
+			System.out.println("2. File Menu options");
+			System.out.println("3. Exit");
+			System.out.println();
+
+
+			Scanner input = new Scanner(System.in);
+			int choice = input.nextInt();
+
+
+			if(choice==1) {
+				ShowAllFiles sf = new ShowAllFiles();
+				sf.show();
+
+			}
+			else if(choice==2) {
+				while(true) {
+					
+					System.out.println("a : Add a new File");
+					System.out.println("b : write in  a new File");
+					System.out.println("c : Delete a file");
+					System.out.println("d : Search a file");
+					System.out.println("e : Exit (from file menu)");
+					
+					String choice1=input.next();
+
+					if(choice1.equals("e")) {
+						System.out.println(" Moved to the main menu ");
+						System.out.println();
+						break;
+					}
+					switch(choice1) {
+
+					case "a":
+						System.out.println(" Enter file name to be added in the folder ");
+						AddOperation af=new AddOperation();
+						af.createFile();
+						break;
+
+					case "b":
+						System.out.println(" Enter file name to written in the folder ");
+						WriteFile wf=new WriteFile();
+						wf.writefile();
+						break;
+
+					case "c":
+						System.out.println(" Enter file name to be deleted from the folder ");
+						DeleteFile df= new DeleteFile();
+						df.delete();
+						break;
+
+
+					case "d":
+						System.out.println(" Enter file name to be seached ");
+						SearchFile sf=new SearchFile();
+						sf.search();
+			
+
+						break;
+						
+                     default:
+                    	 System.out.println("Please enter a correct choice");
+                    	 System.out.println();
+                    	 
+
+					}
+
+				}
+
+			}
+			else if(choice==3) {
+				System.out.println(" Thanks for using Lockedme.com services. ");
+				System.out.println();
+				break;
+
+			}
+			else {
+				System.out.println(" Please Enter a Correct Choice "); 
+				System.out.println();
+			}
+
+
+
+
+
+		}
+
+	}
+}
